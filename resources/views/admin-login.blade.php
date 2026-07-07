f1fd595aa96c2ef3d5c005a75a3175ced909902b
Shanks0789 <villaceranshandy7@gmail.com>
2026-07-07 21:27:02 +0800

    Fix formatting of primary color declaration

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <style>
         :root {
-            --primary: #5D4037; 
+            --primary: #5D4037;  
             --secondary: #D7CCC8;
             --accent: #8D6E63;
             --bg: #EFEBE9;
